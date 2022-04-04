e9c560f0f8e54da25cbed1bc09ddf444048c46d5
mcgraneder <mcgrane480@gmail.com>
2022-04-04 14:30:04 +0100

    debugging production2

--- src/pages/WalletPage.js
+++ src/pages/WalletPage.js
@@ -189,7 +189,7 @@ const WalletPage = () => {
                 amount={text} 
                 visible={pending1}
             />
-        <ConfirmationModal
+        {/* <ConfirmationModal
                 close={() => setConfirm(!confirm)} 
                 amount={text} 
                 visible={confirm}
@@ -200,7 +200,7 @@ const WalletPage = () => {
                 }
                 TransactionType={setTransactionType}
                 gass={gas}
-            />
+            /> */}
             <TransactionSubmittedModal
                 close={() => closeSbmissionModal()} 
                 amount={text} 
@@ -212,10 +212,10 @@ const WalletPage = () => {
                 visible={rejected}
             />
         
-            {/* <TokenListModal 
+            <TokenListModal 
                 visible={showTokenModal} 
                 close={toggleTokenModal}
-            /> */}
+            />
             <WalletModal  
                 setConfirm={setConfirm}
                 setText={setText}
